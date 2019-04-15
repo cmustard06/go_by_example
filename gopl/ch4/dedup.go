ab474a72a390ea3fb7906825964c9fe1aa9c6d92
cmustard06 <cmustard06@gmail.com>
2019-04-15 15:47:58 +0800

    打印输出的每一行，如果是重复的就不输出

---
+++ gopl/ch4/dedup.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+/*
+打印输出的每一行，如果是重复的就不输出
+*/
+
+func main(){
+	seen := make(map[string]bool)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan(){
+		line := input.Text()
+        if !seen[line]{
+        	seen[line] = true
+        	fmt.Println(line)
+		}
+	}
+	if err:= input.Err();err!=nil{
+		fmt.Fprintf(os.Stderr,"dedup: %v \n",err)
+		os.Exit(1)
+	}
+	fmt.Println(seen)
+}
