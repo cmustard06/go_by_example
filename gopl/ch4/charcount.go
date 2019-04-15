a95e8d8f0adf89cd4a87b3a7b5584b16137e9f6e
cmustard06 <cmustard06@gmail.com>
2019-04-15 15:35:57 +0800

    计算输入流中的unicode字符个数*

---
+++ gopl/ch4/charcount.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"unicode/utf8"
+	"bufio"
+	"os"
+	"io"
+	"fmt"
+	"unicode"
+)
+/*计算输入流中的unicode字符个数*/
+
+func main(){
+	counts := make(map[rune]int)
+	var utflen [utf8.UTFMax+1]int
+	invaild := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for{
+		r,n,err := in.ReadRune()
+		if err==io.EOF{
+			break
+		}
+
+		if err !=nil{
+			fmt.Fprintf(os.Stderr,"charcount:%v\n",err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n==1{
+			invaild++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c,n :=range counts{
+		fmt.Printf("%q\t%d\n",c,n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i,n:= range utflen{
+		if i>0{
+			fmt.Printf("%d\t%d\n",i,n)
+		}
+	}
+	if invaild>0{
+		fmt.Printf("\n%d invalid UTF-8 characters\n",invaild)
+	}
+}
