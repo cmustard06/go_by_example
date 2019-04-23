163975e961b70ea357a69831b2e1cc5282286723
cmustard06 <cmustard06@gmail.com>
2019-04-23 21:34:44 +0800

    //单个页面抓取链接

--- gopl/ch5/findlinks1.go
+++ gopl/ch5/findlinks1.go
@@ -1,47 +1,46 @@
 package main
 
 import (
-	"golang.org/x/net/html"
-	"os"
-	"fmt"
-	"net/http"
 	"crypto/tls"
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
 	"time"
 )
 
-//从一个HTML文档中抓取href，并输出
+//单个页面抓取链接
 
-
-func visit(links []string, n *html.Node)[]string{
-	if n.Type == html.ElementNode && n.Data == "a"{
-		for _,a := range n.Attr{
-			if a.Key == "href"{
-				//fmt.Println(a.Val)
-				links = append(links,a.Val)
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
 			}
 		}
 	}
-	for c := n.FirstChild;c!=nil;c=c.NextSibling{
-		links = visit(links,c)
+	//迭代查询href直到在本页中查询完成
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+		//fmt.Println(c.Attr)
 	}
 	return links
 }
 
-func main(){
-	tr := &http.Transport{TLSClientConfig:&tls.Config{InsecureSkipVerify:false}}
-	client := &http.Client{Transport:tr, Timeout:time.Second*2}
-	resp,err := client.Get("https://docs.python.org/3.7/library/index.html")
-	if err!=nil{
-		fmt.Errorf("get error %v",err)
+func main() {
+	tr := http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: false}}
+	client := http.Client{Transport: &tr, Timeout: time.Second * 4}
+	resp, err := client.Get("https://www.csdn.net")
+	if err != nil {
+		fmt.Errorf("get error: %#v", err)
 	}
 	defer resp.Body.Close()
-
-	doc,err := html.Parse(resp.Body)
-	if err!=nil{
-		fmt.Fprintf(os.Stderr,"findlinks1:%v\n",err)
-		os.Exit(1)
+	node, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Errorf("html parse err:%#v", err)
 	}
-	for _,link := range visit(nil,doc){
+	links := visit(nil, node)
+	for _, link := range links {
 		fmt.Println(link)
 	}
+
 }
